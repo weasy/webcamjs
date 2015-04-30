703d019ff849ea94a8d6e4113c19610edc81b22f
weasy <jim@weasy.net>
2015-04-30 16:39:12 -0500

    test

--- weasysnap/annotate.php
+++ weasysnap/annotate.php
@@ -1,20 +1,22 @@
 
 <?php
 
-// convert flower.jpg -font courier -fill white -pointsize 20 -annotate +50+50 Flower flower_annotate1.jpg
+// convert flower.jpg -fill white -box "#00770080" -gravity South -pointsize 20 -annotate +0+5 "   Flower  " flower_annotate2.jpg
 
 $resource = NewMagickWand();
 $dwand = NewDrawingWand();
 $pwand = NewPixelWand();
 
-PixelSetColor($pwand, "black");
+PixelSetColor($pwand, "white");
 DrawSetFont($dwand, "/usr/share/fonts/msttcorefonts/verdana.ttf");
 DrawSetFontSize($dwand, 20);
 DrawSetFillColor($dwand, $pwand);
 
+DrawSetGravity($dwand, MW_SouthGravity);
+
 MagickReadImage( $resource, 'small_flower.jpg' );
 
-if( MagickAnnotateImage( $resource, $dwand, 0, 0, 0, "Text" ) )
+if( MagickAnnotateImage( $resource, $dwand, 0, 0, 0, "Flower" ) )
 {
   header( 'Content-Type: image/gif' );
   MagickEchoImageBlob( $resource );
