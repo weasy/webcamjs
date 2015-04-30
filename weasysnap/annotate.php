429c746065517185bfee1d0029ffdaacc5fdcc8e
weasy <jim@weasy.net>
2015-04-30 16:48:22 -0500

    test

--- weasysnap/annotate.php
+++ weasysnap/annotate.php
@@ -1,29 +1,19 @@
-
 <?php
-
-// convert flower.jpg -fill white -box "#00770080" -gravity South -pointsize 20 -annotate +0+5 "   Flower  " flower_annotate2.jpg
-
-$resource = NewMagickWand();
-$dwand = NewDrawingWand();
-$pwand = NewPixelWand();
-
-PixelSetColor($pwand, "white");
-DrawSetFont($dwand, "/usr/share/fonts/msttcorefonts/verdana.ttf");
-DrawSetFontSize($dwand, 20);
-DrawSetFillColor($dwand, $pwand);
-
-DrawSetGravity($dwand, MW_SouthGravity);
-
-MagickReadImage( $resource, 'small_flower.jpg' );
-
-if( MagickAnnotateImage( $resource, $dwand, 0, 0, 0, "Flower" ) )
-{
-  header( 'Content-Type: image/gif' );
-  MagickEchoImageBlob( $resource );
-}
-else
-{
-  echo MagickGetExceptionString($resource);
-}
-
+  $magick_wand=NewMagickWand();
+  MagickReadImage($magick_wand,'rose.jpg');
+  $drawing_wand=NewDrawingWand();
+  DrawSetFont($drawing_wand,"/usr/share/fonts/bitstream-vera/Vera.ttf");
+  DrawSetFontSize($drawing_wand,20);
+  DrawSetGravity($drawing_wand,MW_CenterGravity);
+  $pixel_wand=NewPixelWand();
+  PixelSetColor($pixel_wand,"white");
+  DrawSetFillColor($drawing_wand,$pixel_wand);
+  if (MagickAnnotateImage($magick_wand,$drawing_wand,0,0,0,"Rose") != 0)
+    {
+      MagickEchoImageBlob( $magick_wand );
+    }
+  else
+    {
+      echo MagickGetExceptionString($magick_wand);
+    }
 ?>
