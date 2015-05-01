6bb502b0a1d699ac2666f5afcac0af4c40d1c8f7
weasy <jim@weasy.net>
2015-04-30 23:22:42 -0500

    draw instead of imagick

--- weasysnap/annotate.php
+++ weasysnap/annotate.php
@@ -13,8 +13,8 @@ function annotateImage($imagePath, $strokeColor, $fillColor) {
 	$text = "today's quote";
 
 	$draw->setFont("/usr/share/fonts/msttcorefonts/impact.ttf");
-	$imagick->setgravity(8);
-	$imagick->annotateimage($draw, 40, 0, 0, $text);
+	$draw->setgravity(imagick::GRAVITY_SOUTH);
+	$imagick->annotateimage($draw, 0, 0, 0, $text);
 
 	header("Content-Type: image/jpg");
 	echo $imagick->getImageBlob();
