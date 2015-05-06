e027f35a54020b874d900aff776253d8c63fed4f
weasy <jim@weasy.net>
2015-05-05 22:45:10 -0500

    upload2 attempt

--- weasysnap/annotate.php
+++ weasysnap/annotate.php
@@ -3,6 +3,7 @@
 function annotateImage($imagePath, $strokeColor, $fillColor) {
 	$imagick = new \Imagick(realpath($imagePath));
 
+	
 	$draw = new \ImagickDraw();
 	$draw->setStrokeColor($strokeColor);
 	$draw->setFillColor($fillColor);
@@ -10,21 +11,32 @@ function annotateImage($imagePath, $strokeColor, $fillColor) {
 	$draw->setStrokeWidth(1);
 	$draw->setFontSize(36);
 	 
-	$text = "NO!!!!!";
+	$text = $_POST["selfxpress"];
 
 	$draw->setFont("/usr/share/fonts/msttcorefonts/impact.ttf");
 	$draw->setgravity(imagick::GRAVITY_SOUTH);
 	$imagick->annotateimage($draw, 0, 0, 0, $text);
-
 	header("Content-Type: image/jpg");
 	echo $imagick->getImageBlob();
-	//setting saved file type here
+	//setting saved file type here. Use date(); to save file with timestamp.jpg
 	$imagick->setimageformat("jpeg");
-	//unlink($your_file);
-	$timestamp = date('D:H:i:s');
-	$imagick->writeImage ($timestamp.".jpg");
+	$timestamp = date('D:H:i');
+	$fileToWrite = "tmp/".$timestamp.".jpg";
+	$imagick->writeImage ($fileToWrite);
+	return $fileToWrite;
+	//$imagick->writeImage ("tmp/".$timestamp.".jpg");
+	
 }
-//outside of function I have to save the image file
-annotateImage("rose.jpg", black, white);
+
+    //if ($_POST["selfxpress"]) {
+	//annotateImage("webcam.jpg", black, white);
+	//echo "tmp/" . $timestamp . ".jpg";
+//}
+
+if ($_POST["selfxpress"]) {
+	$imageLocation = annotateImage("webcam.jpg", black, white);
+	echo '<img src="' . $imageLocation . '" />';
+}
+echo "test!!!";
 
 ?>
