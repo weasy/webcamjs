94e4bfa977831f34e500bafd2d09ad391ade2578
weasy <jim@weasy.net>
2015-05-07 22:17:32 -0500

    Major changes!!!

--- weasysnap/annotate2.php
+++ weasysnap/annotate2.php
@@ -1,18 +1,14 @@
 <?php 
-
 function annotateImage($imagePath, $strokeColor, $fillColor) {
 	$imagick = new \Imagick(realpath($imagePath));
-
 	
 	$draw = new \ImagickDraw();
 	$draw->setStrokeColor($strokeColor);
 	$draw->setFillColor($fillColor);
-
 	$draw->setStrokeWidth(1);
 	$draw->setFontSize(36);
 	 
 	$text = $_POST["selfxpress"];
-
 	$draw->setFont("/usr/share/fonts/msttcorefonts/impact.ttf");
 	$draw->setgravity(imagick::GRAVITY_SOUTH);
 	$imagick->annotateimage($draw, 0, 0, 0, $text);
@@ -27,16 +23,13 @@ function annotateImage($imagePath, $strokeColor, $fillColor) {
 	//$imagick->writeImage ("tmp/".$timestamp.".jpg");
 	
 }
-
     //if ($_POST["selfxpress"]) {
 	//annotateImage("webcam.jpg", black, white);
 	//echo "tmp/" . $timestamp . ".jpg";
 //}
-
 if ($_POST["selfxpress"]) {
 	$imageLocation = annotateImage("webcam.jpg", black, white);
 	echo '<img src="' . $imageLocation . '" />';
 }
 echo "test!!!";
-
 ?>
